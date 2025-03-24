e61f34fefe6aa90c8e3c950e053a354faa001c66
filippo <filippo.gelmetti@gmail.com>
2025-03-24 23:47:50 +0000

    selecting node

--- react/src/mighty-widget/src/presentation/components/RingNode/RingNode.tsx
+++ react/src/mighty-widget/src/presentation/components/RingNode/RingNode.tsx
@@ -13,22 +13,23 @@ const RingNode: React.FC<RingNodeProps> = ({ node, x, y }) => {
 
   console.log(`selectedNode 🏄‍♂️🏄‍♂️🏄‍♂️🏄‍♂️: ${selectedNode?.SSID}`);
 
+  const isSelected = selectedNode && selectedNode.SSID === node.SSID;
+
   return (
     <>
-      <circle cx={x} cy={y} r={20} fill={node.fill} />
-      <circle
-        cx={x}
-        cy={y}
-        r={15}
-        fill="red"
+      <g
+        className={`ring-node ${isSelected ? "selected" : ""}`}
         onMouseEnter={() => {
           setSelectedNode(node);
           console.log("ENTER CIRCLE 🔴");
         }}
-      />
-      <text x={x} y={y + 30} textAnchor="middle">
-        {node.SSID}
-      </text>
+      >
+        <circle cx={x} cy={y} r={20} className="ring-node-border" />
+        <circle cx={x} cy={y} r={15} className="ring-node-core" />
+        <text x={x} y={y + 30} textAnchor="middle">
+          {node.SSID}
+        </text>
+      </g>
     </>
   );
 };
